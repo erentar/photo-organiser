0c72638995101d8fcd1c2175501df5a15ecfb710
erentar <erentar2002@gmail.com>
2024-09-25 15:14:58 +0200

    cleanup

--- organise.sh
+++ organise.sh
@@ -25,16 +25,11 @@ function move_file {
 	_PATH="$DESTPATH/$_YEAR/$_DIRNAME"
 
 	mkdir -p "$_PATH"
-	
-	# move the original
-	# mv "$_FILEPATH" "$_PATH/"
 
 	# move everything including sidecars
 	_PARENTDIR=$(dirname "$_FILEPATH")
 	_BASENAME1=$(basename "$_FILEPATH")
 	_BASENAME="${_BASENAME1%.*}"
-	# echo "$_BASENAME"
-	# echo "$_PARENTDIR"
 
 	mv --backup=t "$_PARENTDIR/$_BASENAME."* "$_PATH/"
 }
@@ -45,14 +40,3 @@ export -f move_file
 export DESTPATH
 
 find "$SRCPATH" -type f -iregex '.*\.\('"$extensions"'\)$' -exec bash -c 'move_file "{}"' \;
-
-# move_file dummy
-# move_sidecar IMG_0528.CR2
-
-# for ext in $PHOTO_EXT;do
-
-# function move_sidecar {
-# 	_FILENAME=$1
-# 	_BASENAME="${_FILENAME%.*}"
-# 	echo $_BASENAME
-# }
